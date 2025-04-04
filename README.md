8ee5791805bf642b2cd90141640919b0a90bd72f
Maria Elisa Gomes de Matos <mariaelisagmt@gmail.com>
2025-04-04 15:24:22 -0300

    STYLE: Button disabled

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Recursos utilizados:
 Angular material icons
 TailwindCss
 Typescript
+
+
+Documentação rxjs: https://dev.to/felipedsc/behaviorsubject-para-comunicacao-entre-componentes-3kpj
--- sintetiza-frontend/src/app/components/input-answer/input-answer.component.ts
+++ sintetiza-frontend/src/app/components/input-answer/input-answer.component.ts
@@ -21,16 +21,23 @@ export class InputAnswerComponent {
 
   public word: FormControl = new FormControl('');
   public words: string[] = [];
+  public limitWord: boolean = false;
 
   constructor(private service: SintetizeService){}
 
   public saveWord() {
-    if(this.word.valid && this.words.length < 5){//adicionar validador especifico e mensagem de erro quando terminar      
+    if(this.word.valid && this.words.length < 5){//todo - adicionar validador especifico e mensagem de erro quando terminar, validar so letras, validar tamanho, validar espaços
       this.words.push(this.word.value);
       this.word = new FormControl('')
       this.saveAnswer();
     }
   }
+
+  public disabledButton(){
+    
+  }
+
+
   
   private saveAnswer(){
     if(this.words.length == 5){  
@@ -46,6 +53,7 @@ export class InputAnswerComponent {
       
       this.question.anwers.push(anwser);
       this.service.saveWord(this.question);
+      this.limitWord = true;
     }
   }
 
