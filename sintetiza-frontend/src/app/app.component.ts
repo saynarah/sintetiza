3bdbb4db0a8a792703d4f3d6507819d17088f3c5
Maria Elisa Gomes de Matos <mariaelisagmt@gmail.com>
2025-04-04 01:22:25 -0300

    FEAT: Add new components

--- sintetiza-frontend/src/app/app.component.ts
+++ sintetiza-frontend/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { HomeComponent } from "./page/home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [ HeaderComponent, FooterComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
