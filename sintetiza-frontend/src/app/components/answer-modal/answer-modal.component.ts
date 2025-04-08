609537ee6b064ef949d61f70248b9faa3807f3b8
Maria Elisa Gomes de Matos <mariaelisagmt@gmail.com>
2025-04-08 00:52:36 -0300

    FEAT: add answer component

---
+++ sintetiza-frontend/src/app/components/answer-modal/answer-modal.component.ts
@@ -0,0 +1,27 @@
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { Component, Input, input, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IQuestion } from '../../interfaces/question.interface';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-answer-modal',
+  imports: [CommonModule, MatDialogModule, MatButtonModule],
+  templateUrl: './answer-modal.component.html',
+  styleUrl: './answer-modal.component.css'
+})
+export class AnswerModalComponent implements OnInit{
+
+  @Input() question: IQuestion = {
+      id: '',
+      description: '',
+      dateTime: new Date(),
+      anwers: []
+    };
+
+  constructor(private matdialog: MatDialog) { }
+
+  ngOnInit(): void {
+    console.log('question', this.question)
+  }
+}
--- sintetiza-frontend/src/app/page/list/list.component.ts
+++ sintetiza-frontend/src/app/page/list/list.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { SintetizeService } from './../../services/sintetize.service';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AnswerModalComponent } from '../../components/answer-modal/answer-modal.component';
+import { IQuestion } from '../../interfaces/question.interface';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-list',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
-export class ListComponent {
+export class ListComponent implements OnInit{
 
+  public questions: IQuestion[] = [];
+
+  constructor(
+    private service: SintetizeService,
+    private dialog: MatDialog
+  ) {}
+
+  ngOnInit(): void {
+    this.service.getQuestions()
+    .subscribe(question => {
+      this.questions = question;
+    })
+  }
+
+  abrirModal(question: IQuestion) {
+    const dialogReference = this.dialog.open(AnswerModalComponent, {
+      width: '400px'
+    });
+
+    dialogReference.componentInstance.question = question;    
+
+  }
 }
