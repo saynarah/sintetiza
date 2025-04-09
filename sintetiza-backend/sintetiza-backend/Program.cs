8cd362c99fa3b06cdfe74aae33161a923cf4f204
Maria Elisa Gomes de Matos <mariaelisagmt@gmail.com>
2025-04-09 00:47:13 -0300

    FIX: add di

--- sintetiza-backend/sintetiza-backend/Program.cs
+++ sintetiza-backend/sintetiza-backend/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using sintetiza_backend.Repository;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
@@ -8,6 +9,8 @@ var host = new HostBuilder()
     {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
+        services.AddSingleton<QuestionService>();
+        services.AddSingleton<TableStorageRepository<QuestionEntity>>();
     })
     .Build();
 
