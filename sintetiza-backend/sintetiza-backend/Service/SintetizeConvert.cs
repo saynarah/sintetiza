5314b87ea89cc96fa8575feca6590c48a068ff47
Maria Elisa Gomes de Matos <mariaelisagmt@gmail.com>
2025-04-10 01:59:39 -0300

    FEAT: restruct services

---
+++ sintetiza-backend/sintetiza-backend/Service/SintetizeConvert.cs
@@ -0,0 +1,55 @@
+﻿using sintetize.Models;
+
+namespace sintetiza_backend.Repository;
+
+public class SintetizeConvert
+{
+    public QuestionResponse ConvertQuestionForResponse(QuestionEntity entity)
+    {
+        return new QuestionResponse(
+            Id: entity.Id,
+            RowKey: entity.RowKey,
+            Description: entity.Description,
+            Date: entity.Date,
+            Answers: entity.Answers.Select(ConvertAnswerForesponse));
+    }
+
+    public AnswerResponse ConvertAnswerForResponse(AnswerEntity entity)
+    {
+        return new AnswerResponse(
+            Id: entity.Id,
+            QuestionPartitionKey: entity.PartitionKey,
+            Words: entity.Words,
+            Actor: entity.Actor);
+    }
+
+    public QuestionEntity ConvertQuestionForEntity(QuestionResponse response)
+    {
+        var entity = new QuestionEntity(
+            id: response.Id,
+            description: response.Description,
+            date: response.Date);
+
+        entity.Answers = response.Answers.Select(x => ConvertAnswerForEntity(x, entity.RowKey));
+
+        return entity;
+    }
+
+    public AnswerEntity ConvertAnswerForEntity(AnswerResponse response, string questionPartitionKey)
+    {
+        return new AnswerEntity(
+            id: response.Id,
+            partitionKey: questionPartitionKey,
+            words: response.Words,
+            actor: response.Actor);
+    }
+
+    public AnswerEntity ConvertAnswerForEntity(AnswerResponse response)
+    {
+        return new AnswerEntity(
+            id: response.Id,
+            partitionKey: response.QuestionPartitionKey,
+            words: response.Words,
+            actor: response.Actor);
+    }
+}
---
+++ sintetiza-backend/sintetiza-backend/Service/SintetizeService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Configuration;
+using sintetize.Models;
+
+namespace sintetiza_backend.Repository;
+
+public class SintetizeService 
+{
+    private readonly TableStorageRepository<QuestionEntity> _questionRepository;
+    private readonly TableStorageRepository<AnswerEntity> _answeRepository;
+    private readonly SintetizeConvert _convert;
+
+    public SintetizeService(IConfiguration configuration, SintetizeConvert convert)
+    {
+        _questionRepository = new TableStorageRepository<QuestionEntity>(configuration, "Questions");
+        _answeRepository = new TableStorageRepository<AnswerEntity>(configuration, "Answers");
+        _convert = convert;
+    }
+
+    public async Task<List<QuestionResponse>> GetAllQuestionAsync()
+    {
+        var entities = await _questionRepository.GetAllAsync();
+
+        return entities.Select(_convert.ConvertQuestionForResponse).ToList();
+    }
+
+    public async Task<QuestionResponse> GetQuestionAsync(string questionId)
+    {
+        var entitie = await _questionRepository.GetAsync("aNS", questionId);
+
+        return _convert.ConvertQuestionForResponse(entitie);
+    }
+
+    public async Task CreateAync(QuestionResponse response)
+    {
+        var questionEntity = _convert.ConvertQuestionForEntity(response);
+        await _questionRepository.AddAsync(questionEntity);
+
+        questionEntity.Answers.Select(_answeRepository.AddAsync);
+
+    }
+
+    public async Task CreateAnswerAync(AnswerResponse response)
+    {
+        var entity = _convert.ConvertAnswerForEntity(response);
+        await _answeRepository.AddAsync(entity);
+    }
+
+}
